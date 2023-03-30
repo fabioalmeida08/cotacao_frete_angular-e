4d697291c05602488b3faa597ecb601a1c1d2691
Fábio <fabioalmeida0896@gmail.com>
2023-03-30 19:55:50 -0300

    feat: created a function to format order number description on freight panel

--- freights/admin.py
+++ freights/admin.py
@@ -3,7 +3,10 @@ from .models import Freight
 
 
 class FreightAdmin(admin.ModelAdmin):
-    list_display = ('order', 'carrier', 'delivery_time', 'delivery_cost')
+    list_display = ('formated_order', 'carrier', 'delivery_time', 'delivery_cost')
 
+    def formated_order(self, obj):
+        return obj.order
+    formated_order.short_description = 'order number'
 
 admin.site.register(Freight, FreightAdmin)

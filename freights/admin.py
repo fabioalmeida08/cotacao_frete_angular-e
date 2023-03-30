b66a198ee3095ddc600745870c90c02d0a4e18c4
Fábio <fabioalmeida0896@gmail.com>
2023-03-30 18:58:26 -0300

    feat: add freight on admin panel and changed display list

--- freights/admin.py
+++ freights/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Freight
 
-# Register your models here.
+
+class FreightAdmin(admin.ModelAdmin):
+    list_display = ('order', 'carrier', 'delivery_time', 'delivery_cost')
+
+
+admin.site.register(Freight, FreightAdmin)

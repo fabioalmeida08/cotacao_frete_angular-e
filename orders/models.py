8fff9e1dff9f5bfee98662e347c7283dad9b3484
Fábio <fabioalmeida0896@gmail.com>
2023-03-30 18:38:01 -0300

    feat: created order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import uuid
+class Order(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    number = models.PositiveBigIntegerField()
+    amount = models.PositiveIntegerField()
+    weight = models.FloatField()
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    length = models.PositiveIntegerField()
+    zip_from = models.CharField(max_length=10)
+    zip_to = models.CharField(max_length=10)
 
-# Create your models here.
+    def __str__(self):
+        return f'{self.number}'

10e8f735a9238648d0e1330d228c69c7fe4dc143
Fábio <fabioalmeida0896@gmail.com>
2023-03-30 19:58:39 -0300

    feat: add function to format freight delivery cost with R$

--- freights/admin.py
+++ freights/admin.py
@@ -3,10 +3,16 @@ from .models import Freight
 
 
 class FreightAdmin(admin.ModelAdmin):
-    list_display = ('formated_order', 'carrier', 'delivery_time', 'delivery_cost')
+    list_display = ('formated_order', 'carrier',
+                    'delivery_time', 'formated_delivery_cost')
 
     def formated_order(self, obj):
         return obj.order
     formated_order.short_description = 'order number'
+
+    def formated_delivery_cost(self, obj):
+        return f'R$ {obj.delivery_cost}'
+    formated_delivery_cost.short_description = 'Delivery Cost'
+
 
 admin.site.register(Freight, FreightAdmin)

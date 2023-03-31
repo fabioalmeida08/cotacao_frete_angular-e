20fc6dd573a7991c8216e996b03cbcb59ddf603d
Fábio <fabioalmeida0896@gmail.com>
2023-03-30 22:10:01 -0300

    chore: add metric system standard melhorenvio API uses on display to orders fields names

--- orders/models.py
+++ orders/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 import uuid
+
+
 class Order(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    number = models.PositiveBigIntegerField()
+    number = models.PositiveBigIntegerField(verbose_name="Order number")
     amount = models.PositiveIntegerField()
-    weight = models.FloatField()
-    width = models.PositiveIntegerField()
-    height = models.PositiveIntegerField()
-    length = models.PositiveIntegerField()
+    weight = models.FloatField(verbose_name="Weight (kg)")
+    width = models.PositiveIntegerField(verbose_name='Width (cm)')
+    height = models.PositiveIntegerField(verbose_name='Height (cm)')
+    length = models.PositiveIntegerField(verbose_name='Lenght in (cm)')
     zip_from = models.CharField(max_length=10)
     zip_to = models.CharField(max_length=10)
 
